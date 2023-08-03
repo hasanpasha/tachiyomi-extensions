3dfca8e2f59c208c9d1baa84840c0802435076cc
hasna pasha <38372270+hasanpasha@users.noreply.github.com>
2023-08-03 17:50:00 +0300

    new extension: hotgirl.asia

---
+++ src/en/hotgirlasia/src/eu/kanade/tachiyomi/extension/en/hotgirlasia/Hotgirlasia.kt
@@ -0,0 +1,110 @@
+package eu.kanade.tachiyomi.extension.en.hotgirlasia
+
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import okhttp3.Request
+import okhttp3.Response
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+class Hotgirlasia : ParsedHttpSource() {
+
+    override val name = "Hotgirlasia"
+
+    override val baseUrl = "https://hotgirl.asia"
+
+    override val lang = "en"
+
+    override val supportsLatest = true
+
+//    override val client: OkHttpClient = network.cloudflareClient
+
+    // Popular
+    override fun popularMangaRequest(page: Int): Request {
+        return GET(HotgirlasiaFilters.buildPopularUrl(baseUrl), headers)
+    }
+
+    override fun popularMangaSelector() =
+        "div.movies-list.movies-list-full div.ml-item"
+
+    override fun popularMangaFromElement(element: Element): SManga =
+        SManga.create().apply {
+            setUrlWithoutDomain(element.select("a.ml-mask.jt").attr("href"))
+            title = element.select("a.ml-mask.jt").attr("oldtitle")
+            thumbnail_url = element.select("img.mli-thumb").attr("src")
+        }
+
+    override fun popularMangaNextPageSelector() = "div.exxx" // There is not next page
+
+    // Latest
+    override fun latestUpdatesRequest(page: Int): Request =
+        GET("$baseUrl/photos/page/$page/", headers)
+
+    override fun latestUpdatesSelector() =
+        popularMangaSelector()
+
+    override fun latestUpdatesFromElement(element: Element): SManga =
+        popularMangaFromElement(element)
+
+    override fun latestUpdatesNextPageSelector() =
+        "ul.pagination li.active + li"
+
+    // Search
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        val filterList = if (filters.isEmpty()) getFilterList() else filters
+
+        return if (query.isBlank()) {
+            val url = HotgirlasiaFilters.buildSearchUrl(baseUrl, filterList)
+            GET(url, headers)
+        } else {
+            val url = "$baseUrl/page/$page/?s=${query.replace(' ', '+')}"
+            GET(url, headers)
+        }
+    }
+
+    override fun searchMangaSelector() =
+        popularMangaSelector()
+
+    override fun searchMangaFromElement(element: Element): SManga =
+        popularMangaFromElement(element)
+
+    override fun searchMangaNextPageSelector() =
+        latestUpdatesNextPageSelector()
+
+    // Details
+    override fun mangaDetailsParse(document: Document) =
+        SManga.create().apply {
+            title = document.select("h3[itemprop=name]").text()
+            description = document.select("div.mvic-info").text().trim()
+            genre = document.select("div#mv-keywords a[rel=tag]").joinToString { it.text() }
+            status = SManga.UNKNOWN
+            thumbnail_url = document.select("div.thumb.mvic-thumb img").attr("src")
+        }
+
+    // Chapters
+    override fun chapterListParse(response: Response): List<SChapter> {
+        return listOf(
+            SChapter.create().apply {
+                name = "images"
+                url = response.request.url.encodedPath + "/?stype=slideshow"
+            },
+        )
+    }
+
+    override fun chapterFromElement(element: Element) = throw UnsupportedOperationException("Not used")
+
+    override fun chapterListSelector() = throw UnsupportedOperationException("Not used")
+
+    // Pages
+    override fun pageListParse(document: Document): List<Page> =
+        document.select("div.carousel-inner div.item img").mapIndexed { index, element ->
+            Page(index, "", element.attr("src"))
+        }
+
+    override fun imageUrlParse(document: Document) = throw UnsupportedOperationException("Not used")
+
+    override fun getFilterList(): FilterList = HotgirlasiaFilters.getFilterList()
+}
---
+++ src/en/hotgirlasia/src/eu/kanade/tachiyomi/extension/en/hotgirlasia/HotgirlasiaFilters.kt
@@ -0,0 +1,106 @@
+package eu.kanade.tachiyomi.extension.en.hotgirlasia
+
+import eu.kanade.tachiyomi.extension.en.hotgirlasia.HotgirlasiaFilters.Companion.findInstance
+import eu.kanade.tachiyomi.source.model.Filter
+import eu.kanade.tachiyomi.source.model.FilterList
+
+class HotgirlasiaFilters() {
+
+    companion object {
+
+        fun getFilterList() = FilterList(
+            PopularFilter(),
+            Filter.Separator(),
+            Filter.Header("Tag has the priority"),
+            ByTagFilter(),
+            BySpecialTags(),
+            ByCountryFilter(),
+        )
+
+        fun buildPopularUrl(baseUrl: String): String {
+            return "$baseUrl/${popularFilterList.first().second}"
+        }
+
+        fun buildSearchUrl(baseUrl: String, filter: FilterList): String {
+            val popularFilter = filter.findInstance<PopularFilter>()!!
+            val tagFilter = filter.findInstance<ByTagFilter>()!!
+            val byCountryFilter = filter.findInstance<ByCountryFilter>()!!
+            val bySpecialTags = filter.findInstance<BySpecialTags>()!!
+
+            val tag = tagFilter.state
+            val popular = popularFilter.state
+            val country = byCountryFilter.state
+            val specialTag = bySpecialTags.state
+
+            return if (tag.isNotBlank()) {
+                "$baseUrl/tag/${tag.replace(' ', '-')}"
+            } else if (byCountryList[country].second.isNotBlank()) {
+                "$baseUrl/${byCountryList[country].second}"
+            } else if (specialTagsList[specialTag].second.isNotBlank()) {
+                "${baseUrl}${specialTagsList[specialTag].second}"
+            } else {
+                "$baseUrl/${popularFilterList[popular].second}"
+            }
+        }
+
+        private val popularFilterList = arrayOf<Pair<String, String>>(
+            Pair("Top Week", "top-week-viewed"),
+            Pair("Top Month", "top-month-viewed/"),
+            Pair("Top Year", "top-year-viewed/"),
+        )
+
+        private class PopularFilter(
+            values: Array<Pair<String, String>> = popularFilterList,
+        ) : Filter.Select<String>("Popular", values.map { it.first }.toTypedArray())
+
+        private val byCountryList = arrayOf(
+            Pair("", ""),
+            Pair("Chinese", "genre/china"),
+            Pair("Korean", "genre/korea"),
+            Pair("Japanese", "genre/japan"),
+            Pair("Taiwanese", "tag/taiwan-beauty-photo"),
+            Pair("Thai", "genre/thai"),
+        )
+
+        private class ByCountryFilter(
+            values: Array<Pair<String, String>> = byCountryList,
+        ) : Filter.Select<String>("Nationality", values.map { it.first }.toTypedArray())
+
+        private class ByTagFilter() : Filter.Text("Tag")
+
+        private val specialTagsList = arrayOf(
+            Pair("Korean Generes", ""),
+            Pair("Korean idols", "/genre/korea/korean-idols/"),
+            Pair("Korean Models", "/genre/korea/korean-models/"),
+            Pair("----------", ""),
+            Pair("XIUREN", "/tag/xiuren/"),
+            Pair("FEILIN", "/tag/feilin"),
+            Pair("HUAYAN", "/tag/huayan"),
+            Pair("LEYUAN", "/tag/leyuan"),
+            Pair("XINGYAN", "/tag/xingyan星颜社"),
+            Pair("YOUMI", "/tag/youmi"),
+            Pair("MFSTAR", "/tag/mfstar"),
+            Pair("UGIRLS", "/tag/ugirls"),
+            Pair("IMISS", "/tag/imiss"),
+            Pair("XIAOYU", "/tag/xiaoyu"),
+            Pair("LIGUI", "/tag/ligui"),
+            Pair("MYGIRL", "/tag/mygirl"),
+            Pair("DKGIRL", "/tag/dkgirl"),
+            Pair("CANDY", "/tag/candy"),
+            Pair("MiStar", "/tag/mistar"),
+            Pair("YouWu", "/tag/youwu尤物馆/"),
+            Pair("Beauty Leg", "/genre/taiwan-hongkong-beauty/"),
+            Pair("Beauty Photo", "/tag/chinese-beauty-photo/"),
+            Pair("Big Chest", "/tag/big-chest-photo/"),
+            Pair("Underwear", "/tag/underwear-beauty-photo/"),
+            Pair("Cosplay", "/tag/cosplay-photo/"),
+            Pair("Maid Photo", "/tag/maid-photo"),
+        )
+
+        private class BySpecialTags(
+            values: Array<Pair<String, String>> = specialTagsList,
+        ) : Filter.Select<String>("Special Tags", values.map { it.first }.toTypedArray())
+
+        private inline fun <reified T> Iterable<*>.findInstance() = find { it is T } as? T
+    }
+}
